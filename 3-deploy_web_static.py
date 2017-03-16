7e0c244a245c1ec1a5ec0e14214c7648975369cc
guptaNswati <sgupta1505@gmail.com>
2017-03-16 07:13:11 +0000

    added files

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -3,8 +3,52 @@
 This module contains Fabric script that creates and distributes an archive
 to the web servers, using the function deploy
 """
-do_pack = __import__('1-pack_web_static').do_pack
-do_deploy = __import__('2-do_deploy_web_static').do_deploy
+
+
+from time import strftime
+from fabric.api import *
+import os
+
+
+env.user = 'ubuntu'
+env.hosts = ['52.71.155.152', '184.72.141.74']
+
+
+def do_pack():
+    """
+    generates a .tgz archive of web_static folder files using fabrics local()
+    """
+    try:
+        local("mkdir -p versions")
+        created = "versions/web_static_{:s}.tgz".format(strftime
+                                                        ("%Y%m%d%H%M%S"))
+        local("tar -czvf {} web_static/".format(created))
+        return created
+    except:
+        return None
+
+
+def do_deploy(archive_path):
+    """
+    distributes an archive to the web server using put
+    """
+    try:
+        path, tar_file = os.path.split(archive_path)
+        data_dir = "/data/web_static/releases/"
+        put(archive_path, "/tmp/")
+        run("sudo mkdir -p {}{}/".format(data_dir, tar_file[:-4]))
+        run("sudo tar -xzf /tmp/{} -C {}{}/".format(
+            tar_file, data_dir, tar_file[:-4]))
+        run("sudo rm /tmp/{}".format(tar_file))
+        run("sudo mv {}{}/web_static/* {}{}/".format(
+            data_dir, tar_file[:-4], data_dir, tar_file[:-4]))
+        run("sudo rm -rf {}{}/web_static".format(data_dir, tar_file[:-4]))
+        run("sudo rm -rf /data/web_static/current")
+        run("sudo ln -s {}{}/ /data/web_static/current".format(
+            data_dir, tar_file[:-4]))
+        return True
+    except:
+        return False
 
 
 def deploy():
